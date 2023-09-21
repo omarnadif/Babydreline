2036e1790dce8be32b8f06fd9646afe53cb58699
Omar.nadif <Omar.nadif2022@campus-eni.fr>
2023-09-21 15:22:14 +0200

    Merge branch 'master' of https://github.com/omarnadif/Babydreline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,43 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
-import Content from "./components/content";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
+interface Client {
+  id: number;
+  nom: string;
+  prenom: string;
+  telephone: string;
+  age: number;
+  ecole_id: number;
+}
 
-const App: React.FC = () => {
+const Content: React.FC = () => {
+  const [clients, setClients] = useState<Client[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("http://localhost:3001/getClients");
+        setClients(result.data);
+      } catch (error) {
+        console.error("Errore durante il recupero dei dati:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <div className="App">
-       <Content />
-      </div>
-      <Footer />
-    </>
+    <div>
+      <h1>Clients</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            {client.nom} {client.prenom}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-export default App;
+export default Content;
