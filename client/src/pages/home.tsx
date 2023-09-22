4cd18fb8597cfa59b139ef9b95afd3a53057f4bc
SbLoris <loris.lebreton@hotmail.com>
2023-09-22 09:24:50 +0200

    gg

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../components/style.css'
+import voiture from '../img/pngegg.png';
+import Footer from "./../components/footer";
+import Navbar from "../components/navbar";
+import Content from "../components/content";
+
+
+const Home = () => {
+  return (
+    <><Navbar /><Content /><Footer /></>
+    
+  )
+}
+
+export default Home
